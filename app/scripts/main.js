637ff5ed7059658ef07a95539714dd80d3ee4355
Joao Figueiredo <lucalanca@hotmail.com>
2014-04-04 14:43:16 +0100

    Adds missing config files and linted scripts.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,11 @@
-$.fn.redraw = function(){
-  $(this).each(function(){
-    var redraw = this.offsetHeight;
-  });
-  return this;
-};
-
+'use strict';
 $(function() {
-  $('.gallery-container').photosetGrid({
-    // Set the gutter between columns and rows
-    gutter: '5px',
-    highresLinks: true,
-    onComplete: function (elem) {
-      $(elem).removeClass('loading');
-    }
-  });
+    $('.gallery-container').photosetGrid({
+        // Set the gutter between columns and rows
+        gutter: '5px',
+        highresLinks: true,
+        onComplete: function (elem) {
+            $(elem).removeClass('loading');
+        }
+    });
 });
